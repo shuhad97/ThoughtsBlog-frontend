6e9b4fe322f98b2ccaa05c72bad8ce0e7f127022
shuhad97 <30422131+shuhad97@users.noreply.github.com>
2020-08-06 01:12:16 +0100

    Register UI work

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-
+import './Register.css'
 
 
 
@@ -18,15 +18,23 @@ const Register = (props) => {
   return (
     <div>
       <h2>Register</h2>
-      <form name = "registerForm" onSubmit={registerHandle}>
-        
-        <TextField name ="username" label = "username" variant="filled" size ="small"  onChange = {({ target }) => setUsername(target.value)}></TextField>    
-        <TextField name ="password" label = "password" type ="password" variant="filled" size ="small"  onChange = {({ target }) => setPassword(target.value)}></TextField>    
-        <TextField name ="confirmPassword"  type ="password" label = "Confirm Password" variant="filled" size ="small" onChange = {({ target }) => setConfirmPasswword(target.value)} ></TextField>    
-        <TextField name ="name" label = "Name/Author" variant="filled" size ="small" onChange = {({ target }) => setName(target.value)} ></TextField>    
 
-        <Button id="registerBtn" type="submit" variant="contained" color ="secondary" > Register </Button>
-
+      <form id = "registerForm" onSubmit={registerHandle}>
+        <ul id = "registerFields">
+          <li>
+            <TextField name ="username" label = "username" variant="filled" size ="small"  onChange = {({ target }) => setUsername(target.value)}></TextField>    
+          </li> 
+          <li>
+            <TextField name ="password" label = "password" type ="password" variant="filled" size ="small"  onChange = {({ target }) => setPassword(target.value)}></TextField>    
+          </li>
+          <li>
+            <TextField name ="confirmPassword"  type ="password" label = "Confirm Password" variant="filled" size ="small" onChange = {({ target }) => setConfirmPasswword(target.value)} ></TextField> 
+          </li>
+           <li> 
+          <TextField name ="name" label = "Name/Author" variant="filled" size ="small" onChange = {({ target }) => setName(target.value)} ></TextField>    
+           </li>  
+           <Button id="registerBtn" type="submit" variant="contained" color ="secondary" > Register </Button>
+        </ul>  
 
             
 
